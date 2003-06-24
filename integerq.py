187cae8fcead383faa660f38b8a42617900ef43e
EGUCHI Takuro <devnull@localhost>
2003-06-24 09:33:12 +0000

    edited by eguchi

---
+++ integerq.py
@@ -0,0 +1,14 @@
+#integerq.py
+import primeq
+
+def integer_q(a):
+    if a < 0:
+       a = - a
+    if a == 1:
+       return 1
+    if a == 0:
+       return 0
+    elif primeq.primeQ(a) == 1:
+       return 2
+    else:
+       return 6
