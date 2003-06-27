1516838d78b09ac1776d806953651cd736352ef0
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-06-27 17:27:31 +0000

    refactoring and bug fixes

--- nzmath/rational.py
+++ nzmath/rational.py
@@ -1,7 +1,6 @@
 #rationalnumber add,sub,mul,div,comp
 
 import ring
-
 from gcd import gcd
 
 def toRational(value):
@@ -12,10 +11,13 @@ def toRational(value):
 
 class Rational:
 
-    def __init__(self,numerator,denominator):
+    def __init__(self, numerator, denominator=1):
         if denominator < 0:
             self.numerator = -numerator
             self.denominator = -denominator
+        elif denominator == 1 and isinstance(numerator, Rational):
+            self.numerator = numerator.numerator
+            self.denominator = numerator.denominator
         else :
             self.numerator = numerator
             self.denominator = denominator    
@@ -30,7 +32,7 @@ class Rational:
             denominator=self.denominator
             return  +Rational(numerator,denominator)
         else:
-            return NotImplemented 
+            return NotImplemented
 
     def __sub__(self,other):
         if isinstance(other, Rational):
@@ -48,11 +50,11 @@ class Rational:
         if isinstance(other, Rational):
             numerator=self.numerator*other.numerator
             denominator=self.denominator*other.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.numerator*other
             denominator=self.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator) 
         else:
             return NotImplemented
 
@@ -60,34 +62,34 @@ class Rational:
         if isinstance(other, Rational):
             numerator=self.numerator*other.denominator
             denominator=self.denominator*other.numerator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.numerator
             denominator=self.denominator*other
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         else:
             return NotImplemented
     def __radd__(self,other):
         if isinstance(other, Rational):
             numerator=self.numerator*other.denominator+self.denominator*other.numerator
             denominator=self.denominator*other.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.numerator+self.denominator*other
             denominator=self.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         else:
-            return NotImplemented 
+            return NotImplemented
 
     def __rsub__(self,other):
         if isinstance(other, Rational):
             numerator=self.numerator*other.denominator-self.denominator*other.numerator
             denominator=self.denominator*other.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.denominator*other-self.numerator
-            denominator=self.denominator            
-            return +Rational(numerator,denominator)             
+            denominator=self.denominator
+            return +Rational(numerator,denominator)
         else:
             return NotImplemented
 
@@ -95,11 +97,11 @@ class Rational:
         if isinstance(other, Rational):
             numerator=self.numerator*other.numerator
             denominator=self.denominator*other.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.numerator*other
             denominator=self.denominator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         else:
             return NotImplemented
 
@@ -107,60 +109,31 @@ class Rational:
         if isinstance(other, Rational):
             numerator=self.numerator*other.denominator
             denominator=self.denominator*other.numerator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         elif isinstance(other, int) or isinstance(other, long):
             numerator=self.denominator*other
             denominator=self.numerator
-            return +Rational(numerator,denominator)             
+            return +Rational(numerator,denominator)
         else:
-            return NotImplemented    
+            return NotImplemented
 
     def __lt__(self,other):
-        if self.numerator*other.denominator < self.denominator*other.numerator:
-            return 1
-        else:
-            return 0
+        return self.compare(other) < 0
 
     def __le__(self,other):
-        if self.numerator*other.denominator <= self.denominator*other.numerator:
-            return 1 
-        else:
-            return 0
+        return self.compare(other) <= 0
 
     def __eq__(self,other):
-        if other in ring.theIntegerRing:
-            if self.numerator == self.denominator * other:
-                return 1
-            else:
-                return 0
-        if self.numerator*other.denominator == self.denominator*other.numerator:
-            return 1
-        else:
-            return 0
+        return self.compare(other) == 0
 
     def __ne__(self,other):
-        if other in ring.theIntegerRing:
-            if self.numerator != self.denominator * other:
-                return 1
-            else:
-                return 0
-        
-        if self.numerator*other.denominator != self.denominator*other.numerator:
-            return 1
-        else:
-            return 0
+        return self.compare(other) != 0
 
     def __gt__(self,other):
-        if self.numerator*other.denominator >= self.denominator*other.numerator:
-            return 1
-        else:
-            return 0
+        return self.compare(other) > 0
 
     def __ge__(self,other):
-        if self.numerator*other.denominator > self.denominator*other.numerator:
-            return 1
-        else:
-            return 0
+        return self.compare(other) >= 0
 
     def __pos__(self):
         g=gcd(self.numerator,self.denominator)
@@ -175,5 +148,12 @@ class Rational:
     def __neg__(self):
         return Rational(-self.numerator, self.denominator)
 
-    def __repr__(self):
+    def __str__(self):
         return str(self.numerator)+"/"+str(self.denominator)
+
+    __repr__ = __str__
+
+    def compare(self, other):
+        if isinstance(other, int) or isinstance(other, long):
+            return self.numerator - self.denominator * other
+        return self.numerator*other.denominator - self.denominator*other.numerator
