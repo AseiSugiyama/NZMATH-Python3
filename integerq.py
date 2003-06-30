a073872744ded7350d99bdc7495815c0fa00540f
EGUCHI Takuro <devnull@localhost>
2003-06-30 13:11:36 +0000

    edited by egu

--- integerq.py
+++ integerq.py
@@ -2,6 +2,11 @@
 import primeq
 
 def integer_q(a):
+    """Judge the integer which you input.
+return 0 if the integer is 0.
+return 1 if the integer is 1.
+return 2 if the integer belongs prime numbers.
+return 6 in the case of others."""
     if a < 0:
         a = - a
     if a == 1:
