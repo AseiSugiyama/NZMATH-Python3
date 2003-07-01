5ef7f812ee5b4a3fe63de808966a2414b8e6de97
EGUCHI Takuro <devnull@localhost>
2003-07-01 10:32:22 +0000

    edited by egu

--- nzmath/bigrandom.py
+++ nzmath/bigrandom.py
@@ -3,16 +3,28 @@ def randrange(start,stop = 0,step = 1):
     """Choose a random item from range([start,] stop[, step]).
 (Return long integer.)"""
     import random
-    if start > stop:
-        v = stop
-        stop = start
-        start = v
-    if step <= 0:
-        return -1
-    elif (stop - start) % step > 0:
-        return long(random.random()*(long(stop - start)+1) / step)*step + start
+    if step == 0:
+        raise ValueError, "zero step for randrange()"
+    elif start != long(start):
+        raise ValueError, "non-integer arg 1 for randrange()"
+    elif stop != long(stop):
+        raise ValueError, "non-integer stop for randrange()"
+    elif step != long(step):
+        raise ValueError, "non-integer step for randrange()"
     else:
-        return long(random.random() * (stop - start) / step)*step + start
+        if step < 0:
+            step = -step
+            v = stop
+            stop = start
+            start = v
+        if start > stop:
+            raise ValueError, "empty range for randrange()"
+        else:
+            if (stop - start) % step != 0:
+                v = long((stop - start)/step) + 1
+            else:
+                v = long((stop - start)/step) 
+            return long(random.random() * v) * step + start   
 def random():
     """Get the next random number in the range [0.0, 1.0)."""
     import random
