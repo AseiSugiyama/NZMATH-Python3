4e02c88fbcb514f400850f464cfab5518de7a571
KUMAKI Koji <devnull@localhost>
2003-07-15 08:10:41 +0000

    mebius -> moebius

--- test/testEuler.py
+++ test/testEuler.py
@@ -7,10 +7,10 @@ class EulerTest (unittest.TestCase):
         assert euler.euler(480) == 128
         assert euler.euler(43**5*87**2) ==(43**4)*42*6*29*28 
         assert euler.euler(1042387*701) == 701*700*1486 
-        assert euler.mebius(1) == 1
-        assert euler.mebius(2*3*5*7*11*13) == 1
-        assert euler.mebius(8*987654345) == 0 
-        assert euler.mebius(999991) == -1        
+        assert euler.moebius(1) == 1
+        assert euler.moebius(2*3*5*7*11*13) == 1
+        assert euler.moebius(8*987654345) == 0 
+        assert euler.moebius(999991) == -1        
 
 def suite():
     suite = unittest.TestSuite()
