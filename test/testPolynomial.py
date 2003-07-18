a6aae94a353b41e1c5f3784ff61768baf59a7e83
EGUCHI Takuro <devnull@localhost>
2003-07-18 13:02:29 +0000

    edited by egu for test

---
+++ test/testPolynomial.py
@@ -0,0 +1,62 @@
+import unittest
+from polynomial import *
+
+# data for debugging
+
+a = Polynomial([1,1],"x")
+
+b = Polynomial([1,-2,3,-4],"x")
+
+c = Polynomial([1,-1,-2],"y")
+
+d = Polynomial([1,1,2],"y")
+
+e = Polynomial([0,1,2,3,4],"z")
+
+f = FlatPolynomial({(0,0):1,(1,0):2,(2,0):3,(1,1):4,(0,3):5},["x","z"])
+
+g = FlatPolynomial({(0,0,0):1,(1,0,0):-2,(1,0,3):3,(1,1,1):-4,(0,2,1):5,(2,2,2):-6},["y","z","x"])
+
+class PolynomialTest(unittest.TestCase):
+    def testAdd(self):
+        sum_1 = Polynomial([2,-1,3,-4],"x")
+        sum_2 = FlatPolynomial({(0,0,0):2,(1,0,0):1,(0,1,0):-2,(3,1,0):3,(0,0,1):1,(1,1,1):-4,(0,0,2):2,(1,0,2):5,(2,2,2):-6,(0,0,3):3,(0,0,4):4},["x","y","z"])
+        assert a + b == sum_1
+        assert a + e + g == sum_2
+
+    def testSub(self):
+        sub_1 = Polynomial([0,2,4],"y")
+        sub_2 = FlatPolynomial({(0,0,0):-1,(1,0,0):1,(2,0,0):3,(0,1,0):2,(3,1,0):-3,(1,0,1):4,(1,1,1):4,(1,0,2):-5,(2,2,2):6,(0,0,3):5},["x","y","z"])
+        assert d - c == sub_1
+        assert f - g - a == sub_2
+
+    def testMul(self):
+        mul_1 = Polynomial([1,0,-1,-4,-4],"y")
+        mul_2 = FlatPolynomial({(0,0,0):1,(1,0,0):2,(2,0,0):3,(0,1,0):-1,(1,1,0):-2,(2,1,0):-3,(0,2,0):-2,(1,2,0):-4,(2,2,0):-6,(1,0,1):4,(1,1,1):-4,(1,2,1):-8,(0,0,3):5,(0,1,3):-5,(0,2,3):-10},["x","y","z"])
+        assert c * d == mul_1
+        assert c * f == mul_2
+
+    def testScalarMul(self):
+        mul_1 = Polynomial([0,3,6,9,12],"z")
+        mul_2 = FlatPolynomial({(0,0,0):-5,(0,1,0):10,(3,1,0):-15,(1,1,1):20,(1,0,2):-25,(2,2,2):30},["x","y","z"])
+        assert e * 3 == mul_1
+        assert g * (-5) == mul_2
+
+    def testDifferentiate(self):
+        deff_1 = Polynomial([1,4,9,16],"z")
+        deff_2 = FlatPolynomial({(2,1,0):9,(0,1,1):-4,(0,0,2):5,(1,2,2):-12},["x","y","z"])
+        assert deff_1 == Polynomial.differentiate(e,"z")
+        assert deff_2 == FlatPolynomial.differentiate(g,"x")
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(PolynomialTest("testAdd"))
+    suite.addTest(PolynomialTest("testSub"))
+    suite.addTest(PolynomialTest("testMul"))
+    suite.addTest(PolynomialTest("testScalarMul"))
+    suite.addTest(PolynomialTest("testDifferentiate"))
+    return suite
+
+if __name__== '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
