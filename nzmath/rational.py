7c3307d5e055e6365716c0a9740e34583539afcd
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-07-18 15:50:16 +0000

    move IntegerRing and RationalField from ring.py to rational.py

--- nzmath/rational.py
+++ nzmath/rational.py
@@ -1,9 +1,13 @@
 #rationalnumber add,sub,mul,div,comp
 
-import ring
 from gcd import gcd
 
 class Rational:
+    """
+
+    Rational is the class of rational numbers.
+
+    """
 
     def __init__(self, numerator, denominator=1):
         if denominator < 0:
@@ -18,95 +22,96 @@ class Rational:
 
     def __add__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator+self.denominator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.denominator + self.denominator*other.numerator
+            denominator = self.denominator*other.denominator
             return  +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator+self.denominator*other
-            denominator=self.denominator
+        elif isIntegerObject(other):
+            numerator = self.numerator + self.denominator*other
+            denominator = self.denominator
             return  +Rational(numerator,denominator)
         else:
             return NotImplemented
 
     def __sub__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator-self.denominator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.denominator - self.denominator*other.numerator
+            denominator = self.denominator*other.denominator
             return +Rational(numerator,denominator) 
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator-self.denominator*other
-            denominator=self.denominator            
+        elif isIntegerObject(other):
+            numerator = self.numerator - self.denominator*other
+            denominator = self.denominator            
             return +Rational(numerator,denominator) 
         else:
             return NotImplemented
 
     def __mul__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.numerator
+            denominator = self.denominator*other.denominator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator*other
-            denominator=self.denominator
+        elif isIntegerObject(other):
+            numerator = self.numerator*other
+            denominator = self.denominator
             return +Rational(numerator,denominator) 
         else:
             return NotImplemented
 
     def __div__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator
-            denominator=self.denominator*other.numerator
+            numerator = self.numerator*other.denominator
+            denominator = self.denominator*other.numerator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator
-            denominator=self.denominator*other
+        elif isIntegerObject(other):
+            numerator = self.numerator
+            denominator = self.denominator*other
             return +Rational(numerator,denominator)
         else:
             return NotImplemented
+
     def __radd__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator+self.denominator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.denominator + self.denominator*other.numerator
+            denominator = self.denominator*other.denominator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator+self.denominator*other
-            denominator=self.denominator
+        elif isIntegerObject(other):
+            numerator = self.numerator + self.denominator*other
+            denominator = self.denominator
             return +Rational(numerator,denominator)
         else:
             return NotImplemented
 
     def __rsub__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator-self.denominator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.denominator - self.denominator*other.numerator
+            denominator = self.denominator*other.denominator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.denominator*other-self.numerator
-            denominator=self.denominator
+        elif isIntegerObject(other):
+            numerator = self.denominator*other - self.numerator
+            denominator = self.denominator
             return +Rational(numerator,denominator)
         else:
             return NotImplemented
 
     def __rmul__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.numerator
-            denominator=self.denominator*other.denominator
+            numerator = self.numerator*other.numerator
+            denominator = self.denominator*other.denominator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.numerator*other
-            denominator=self.denominator
+        elif isIntegerObject(other):
+            numerator = self.numerator*other
+            denominator = self.denominator
             return +Rational(numerator,denominator)
         else:
             return NotImplemented
 
     def __rdiv__(self,other):
         if isinstance(other, Rational):
-            numerator=self.numerator*other.denominator
-            denominator=self.denominator*other.numerator
+            numerator = self.numerator*other.denominator
+            denominator = self.denominator*other.numerator
             return +Rational(numerator,denominator)
-        elif isinstance(other, int) or isinstance(other, long):
-            numerator=self.denominator*other
-            denominator=self.numerator
+        elif isIntegerObject(other):
+            numerator = self.denominator*other
+            denominator = self.numerator
             return +Rational(numerator,denominator)
         else:
             return NotImplemented
@@ -130,12 +135,12 @@ class Rational:
         return self.compare(other) >= 0
 
     def __pos__(self):
-        g=gcd(self.numerator,self.denominator)
-        if g != 1:
-            self.numerator=self.numerator//g
-            self.denominator=self.denominator//g
+        commonDivisor = gcd(self.numerator,self.denominator)
+        if commonDivisor != 1:
+            self.numerator //= commonDivisor
+            self.denominator //= commonDivisor
         if self.denominator == 1:
-            return self.numerator
+            return Integer(self.numerator)
         else:
             return Rational(self.numerator, self.denominator)
 
@@ -146,11 +151,152 @@ class Rational:
         return +Rational(abs(self.numerator), self.denominator)
 
     def __str__(self):
-        return str(self.numerator)+"/"+str(self.denominator)
+        return str(self.numerator) + "/" + str(self.denominator)
 
-    __repr__ = __str__
+    def __repr__(self):
+        return "Rational(" + str(self.numerator) + ", " + str(self.denominator) + ")"
 
     def compare(self, other):
-        if isinstance(other, int) or isinstance(other, long):
+        if isIntegerObject(other):
             return self.numerator - self.denominator * other
         return self.numerator*other.denominator - self.denominator*other.numerator
+
+class RationalField:
+    """
+
+    RationalField is a class of field of rationals.
+    The class has the single instance 'theRationalField'.
+
+    """
+
+    def __contains__(self, element):
+        reduced = +element
+        return (isinstance(reduced, Rational) or isIntegerObject(reduced))
+
+    def classNumber(self):
+        """The class number of the rational field is one."""
+        return 1
+
+    def getQuotientField(self):
+        """getQuotientField returns the rational field itself."""
+        return self
+
+    def createElement(self, numerator, denominator=1):
+        """
+
+        createElement returns a Rational object.
+        If the number of arguments is one, it must be an integer or a rational.
+        If the number of arguments is two, they must be integers.
+
+        """
+        return Rational(numerator, denominator)
+
+theRationalField = RationalField()
+
+class Integer(long):
+    """
+
+    Integer is a class of integer.  Since 'int' and 'long' do not
+    return rational for division, it is needed to create a new class.
+
+    """
+
+    def __div__(self, other):
+        if other in theIntegerRing:
+            return +Rational(self, +other)
+        else:
+            return NotImplemented
+
+    def __rdiv__(self, other):
+        if other in theIntegerRing:
+            return +Rational(+other, self)
+        else:
+            return NotImplemented
+
+    __truediv__ = __div__
+
+    __rtruediv__ = __rdiv__
+
+    def __floordiv__(self, other):
+        return Integer(long(self)//other)
+
+    def __rfloordiv__(self, other):
+        return Integer(other//long(self))
+
+    def __mod__(self, other):
+        return Integer(long(self)%other)
+
+    def __rmod__(self, other):
+        return Integer(other%long(self))
+
+    def __divmod__(self, other):
+        return tuple(map(Integer, divmod(long(self), other)))
+
+    def __rdivmod__(self, other):
+        return tuple(map(Integer, divmod(other, long(self))))
+
+    def __add__(self, other):
+        return Integer(long(self)+other)
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        return Integer(long(self)-other)
+
+    def __rsub__(self, other):
+        return Integer(other-long(self))
+
+    def __mul__(self, other):
+        return Integer(long(self)*other)
+
+    __rmul__ = __mul__
+
+    def __pow__(self, other, modulo=None):
+        return Integer(pow(long(self), other, modulo))
+
+    def __rpow__(self, other, modulo=None):
+        return Integer(pow(other, long(self), modulo))
+
+    def __pos__(self):
+        return Integer(self)
+
+    def __neg__(self):
+        return Integr(-long(self))
+
+    def __abs__(self):
+        return Integer(abs(long(self)))
+
+    def getRing(self):
+        return theIntegerRing
+
+class IntegerRing:
+    """
+
+    IntegerRing is a class of ring of rational integers.
+    The class has the single instance 'theIntegerRing'.
+
+    """
+
+    def __contains__(self, element):
+        """
+
+        `in' operator is provided for checking an object be in the
+        rational integer ring mathematically.  To check an object be
+        an integer object in Python, please use isIntegerObject.
+
+        """
+        return isIntegerObject(+element)
+
+    def getQuotientField(self):
+        """getQuotientField returns the rational field."""
+        return theRationalField
+
+    def createElement(self, seed):
+        """createElement returns an Integer object with seed,
+        which must be an integer."""
+        return Integer(seed)
+
+theIntegerRing = IntegerRing()
+
+def isIntegerObject(anObject):
+    return (isinstance(anObject, int) or isinstance(anObject, long))
