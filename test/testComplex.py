f8aa7c8ddc76db977c0cc48908860779e3aad440
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-09-24 09:29:13 +0000

    fix bugs and add exp

--- test/testComplex.py
+++ test/testComplex.py
@@ -1,11 +1,13 @@
 import unittest
 import complex
+import real
 
 class ComplexTest (unittest.TestCase):
     def testAdd(self):
         a = complex.Complex(1, 1)
         b = complex.Complex(complex.pi + 1, 1)
         assert b == a + complex.pi
+
     def testInverse(self):
         a = complex.Complex(1, 1)
         assert a == a.inverse().inverse()
@@ -15,6 +17,16 @@ class ComplexTest (unittest.TestCase):
         b = complex.Complex(1, -1)
         assert a.conjugate() == b
         assert a == a.conjugate().conjugate()
+
+    def testWithFloat(self):
+        a = complex.Complex(8, 1)
+        b = real.Float(1, -3)
+        a_add_b = complex.Complex(8 + real.Float(1, -3), 1)
+        a_mul_b = complex.Complex(1, real.Float(1, -3))
+        assert a_add_b == a + b
+        assert a_add_b == b + a
+        assert a_mul_b == a * b
+        assert a_mul_b == b * a
 
 def suite():
     suite = unittest.makeSuite(ComplexTest, 'test')
