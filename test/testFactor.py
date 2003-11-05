2e383007fa7adb45fd905f15d57ebee99863e4fc
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-11-05 14:53:30 +0000

    move pmom to factor.py

--- test/testFactor.py
+++ test/testFactor.py
@@ -1,5 +1,5 @@
 import unittest
-import factor, pmom
+import factor
 
 class FactorTest (unittest.TestCase):
     def testTrialDivision(self):
@@ -8,8 +8,8 @@ class FactorTest (unittest.TestCase):
         assert factor.trialDivision(200819) == [(409,1),(491,1)]
         assert factor.trialDivision(1042387) ==  [(701,1),(1487,1)]
     def testPMinusOneMethod(self):
-        assert [(19,1), (101,1)] == pmom.pmom(1919)
-        
+        assert [(19,1), (101,1)] == factor.pmom(1919)
+
 def suite():
     suite = unittest.makeSuite(FactorTest, 'test');
     return suite
