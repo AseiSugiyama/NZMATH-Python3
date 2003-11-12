36edc73681ee7e071c9b0b74d08017f7fadf8a42
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-11-12 04:12:40 +0000

    smaller example

--- test/testFactor.py
+++ test/testFactor.py
@@ -1,5 +1,6 @@
 import unittest
 import factor
+import prime
 
 class FactorTest (unittest.TestCase):
     def testTrialDivision(self):
@@ -9,7 +10,9 @@ class FactorTest (unittest.TestCase):
         assert factor.trialDivision(1042387) ==  [(701,1),(1487,1)]
     def testPMinusOneMethod(self):
         assert [(19,1), (101,1)] == factor.pmom(1919)
-        assert [(3L, 1), (20323L, 1), (415077371L, 1), (7040722789L, 1)] == factor.pmom(178178531231211235719711)
+        # 6133 = prime.prime(800) > sqrt(B) & 800 == 0 mod 20
+        p = 4 * 6133 + 1
+        assert [(p,1), (154858631,1)] == factor.pmom(p*154858631)
 
 def suite():
     suite = unittest.makeSuite(FactorTest, 'test');
