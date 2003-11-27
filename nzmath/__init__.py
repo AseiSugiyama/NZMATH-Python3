ccffe7b1941aaa6c5d27c02cd354070c93907f69
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-11-27 15:56:38 +0000

    make __add__ list

---
+++ nzmath/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    "bigrandom",
+    "euler",
+    "factor",
+    "gcd",
+    "imaginary",
+    "integerResidueClass",
+    "lattice",
+    "matrix",
+    "polynomial",
+    "prime",
+    "rational",
+    "rationalFunction",
+    "real",
+    "ring",
+    "vector"
+    ]
