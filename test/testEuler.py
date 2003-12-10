0d876fcdf7de5f4cb888826e1161f7250d5ee3a1
MATSUI Tetsushi <mft@users.sourceforge.net>
2003-12-10 16:54:03 +0000

    clarify test code

--- test/testEuler.py
+++ test/testEuler.py
@@ -5,12 +5,12 @@ class EulerTest (unittest.TestCase):
     def runTest(self):
         assert euler.euler(101) == 100
         assert euler.euler(480) == 128
-        assert euler.euler(43**5*87**2) ==(43**4)*42*6*29*28 
-        assert euler.euler(1042387*701) == 701*700*1486 
+        assert euler.euler(3**2 * 29**3 * 43**5) == 2*3 * 28*29**2 * 42*43**4 
+        assert euler.euler(701**2 * 1487) == 700*701 * 1486
         assert euler.moebius(1) == 1
         assert euler.moebius(2*3*5*7*11*13) == 1
-        assert euler.moebius(8*987654345) == 0 
-        assert euler.moebius(999991) == -1        
+        assert euler.moebius(8*987654345) == 0
+        assert euler.moebius(999991) == -1
 
 def suite():
     suite = unittest.TestSuite()
