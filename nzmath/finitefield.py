602afc0a0e14d568b761fa5c382b7b18efeffb7b
MATSUI Tetsushi <mft@users.sourceforge.net>
2004-01-27 09:47:31 +0000

    implement finite prime field

---
+++ nzmath/finitefield.py
@@ -0,0 +1,96 @@
+"""
+
+finite fields.
+
+"""
+import gcd
+import prime
+import rational
+import ring
+
+class FiniteField (ring.Field):
+    pass
+
+class FiniteFieldElement (ring.FieldElement):
+    pass
+
+import integerResidueClass
+
+class FinitePrimeFieldElement(integerResidueClass.IntegerResidueClass):
+    def __init__(self, representative, modulus):
+        if modulus < 0:
+            modulus = -modulus
+        if prime.primeq(modulus):
+            self.m = modulus
+        else:
+            raise ValueError, "modulus must be a prime."
+        if isinstance(representative, rational.Rational):
+            t = gcd.extgcd(representative.denominator, self.m)
+            if t[2] != 1:
+                raise ValueError, "No inverse of %s." % representative
+            self.n = (representative.numerator * t[0]) % self.m
+        else:
+            self.n = representative % self.m
+
+    def __repr__(self):
+        return "FinitePrimeFieldElement(%d, %d)" % (self.n, self.m)
+
+    def __str__(self):
+        return "%d in F_%d" % (self.n, self.m)
+
+    def getRing(self):
+        return FinitePrimeField(self.m)
+
+class FinitePrimeField (FiniteField):
+    """
+
+    FinitePrimeField is also known as F_p or GF(p).
+
+    """
+    def __init__(self, characteristic):
+        self.char = characteristic
+        self.properties = ring.CommutativeRingProperties()
+        self.properties.setIsfield(True)
+
+    def getCharacteristic(self):
+        """
+
+        Return the characteristic of the field.
+
+        """
+        return self.char
+
+    def __eq__(self, other):
+        if isinstance(other, FinitePrimeField) and self.char == other.char:
+            return True
+        return False
+
+    def issubring(self, other):
+        """
+
+        Report whether another ring contains the field as a subring.
+
+        """
+        if self == other:
+            return True
+        elif isinstance(other, FiniteField) and other.getCharacteristic() == self.char:
+            return True
+        return False
+
+    def issuperring(self, other):
+        """
+
+        Report whether the field is a superring of another ring.
+        Since the field is a prime field, it can be a superring of
+        itself only.
+
+        """
+        if self == other:
+            return True
+        return False
+
+    def __contains__(self, elem):
+        if isinstance(elem, FinitePrimeFieldElement) and elem.getModulus() == self.char:
+            return True
+        return False
+
---
+++ test/testFiniteField.py
@@ -0,0 +1,55 @@
+import unittest
+from finitefield import *
+from rational import Integer
+from rational import Rational
+
+class FinitePrimeFieldElementTest(unittest.TestCase):
+    def testInit(self):
+        elem = FinitePrimeFieldElement(12, 17)
+        assert elem
+        assert elem.toInteger() == 12
+        assert elem.getModulus() == 17
+        residue = FinitePrimeFieldElement(Rational(8,15), 11)
+        assert residue
+        assert residue.toInteger() == 2
+        assert residue.getModulus() == 11
+
+    def testMul(self):
+        residue1 = FinitePrimeFieldElement(8, 151)
+        residue2 = FinitePrimeFieldElement(2, 151)
+        assert residue1 * residue2
+        assert (residue1 * residue2).toInteger() == 16
+        assert (residue1 * residue2).getModulus() == 151
+        assert residue1 * 2
+        assert (residue1 * 2).toInteger() == 16
+        assert (residue1 * 2).getModulus() == 151
+        assert 2 * residue1
+        assert (2 * residue1).toInteger() == 16
+        assert (2 * residue1).getModulus() == 151
+        assert residue1 * Rational(1, 76)
+        assert (residue1 * Rational(1, 76)).toInteger() == 16
+        assert (residue1 * Rational(1, 76)).getModulus() == 151
+        assert Rational(1, 76) * residue1
+        assert (Rational(1, 76) * residue1).toInteger() == 16
+        assert (Rational(1, 76) * residue1).getModulus() == 151
+
+    def testGetRing(self):
+        elem = FinitePrimeFieldElement(12, 17)
+        f17 = elem.getRing()
+        assert isinstance(f17, FinitePrimeField)
+        assert elem in f17
+
+class FinitePrimeFieldTest(unittest.TestCase):
+    def testEq(self):
+        assert FinitePrimeField(17) == FinitePrimeField(17)
+
+def suite():
+    suite = unittest.TestSuite((
+        unittest.makeSuite(FinitePrimeFieldElementTest, 'test'),
+        unittest.makeSuite(FinitePrimeFieldTest, 'test'),
+        ))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
