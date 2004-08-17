2ca8b7b8344f0eb8faebd2c93a8caf74ae33f772
MATSUI Tetsushi <mft@users.sourceforge.net>
2004-08-17 07:26:50 +0000

    code cleaning

--- nzmath/equation.py
+++ nzmath/equation.py
@@ -5,39 +5,39 @@ import gcd
 import arith1
 
 def e2(x):
-    a=x[0]    
-    b=x[1]
-    c=x[2]
-    return ((-b+cmath.sqrt(b**2-4*a*c))/(2*a),((-b-cmath.sqrt(b**2-4*a*c))/(2*a))
+    a = x[0]
+    b = x[1]
+    c = x[2]
+    sqrtd = cmath.sqrt(b**2 - 4*a*c)
+    return ((-b + sqrtd)/(2*a), (-b - sqrtd)/(2*a))
 
 def e2Fp(x,p): # p is prime
     a=x[0]%p    
     b=x[1]%p
     c=x[2]%p
     a=arith1.inverse(2*a,p)
-    c=arith1.sqroot(b**2-4*a*c,p)
-    return (((-b+c)*a)%p,((-b-c)*a)%p)
+    sqrtd = arith1.sqroot(b**2-4*a*c,p)
+    return (((-b+sqrtd)*a)%p,((-b-sqrtd)*a)%p)
 
 def e3(x):
-    a=x[1]/x[0]
-    b=x[2]/x[0]
-    c=x[3]/x[0]
-    p=b-(a**2)/3
-    q=2*(a**3)/27-a*b/3+c
-    w=(-1+cmath.sqrt(3)*1j)/2
-    k=-q/2+math.sqrt((q**2)/4+(p**3)/27)
-    l=-q/2-math.sqrt((q**2)/4+(p**3)/27)
-    if k<0:
-       m=-math.pow(abs(k),1/3)
+    a = x[1]/x[0]
+    b = x[2]/x[0]
+    c = x[3]/x[0]
+    p = b - (a**2)/3
+    q = 2*(a**3)/27 - a*b/3 + c
+    w = ( -1 + cmath.sqrt(-3)) / 2
+    k = -q/2 + math.sqrt((q**2)/4 + (p**3)/27)
+    l = -q/2 - math.sqrt((q**2)/4 + (p**3)/27)
+    if k < 0:
+        m = -math.pow(abs(k), 1/3)
     else:
-	m=math.pow(k,1/3)
-    n=-math.pow(abs(l),1/3)
-    equ=[]
-    i=0
-    while i<=2:
-	x=(w**i)*m +(w**(3-i))*n-a/3
-	equ.append(x)
-	i=i+1
+        m = math.pow(k, 1/3)
+    n = -math.pow(abs(l), 1/3)
+    equ = []
+
+    for i in range(3):
+        x = (w**i)*m +(w**(3-i))*n - a/3
+        equ.append(x)
     return equ
 
 def e3Fp(x,p): # p is prime
@@ -53,14 +53,15 @@ def e3Fp(x,p): # p is prime
     k=-q/2+math.sqrt((q**2)/4+(p**3)/27)
     l=-q/2-math.sqrt((q**2)/4+(p**3)/27)
     if k<0:
-       m=-math.pow(abs(k),1/3)
+        m=-math.pow(abs(k),1/3)
     else:
-	m=math.pow(k,1/3)
+        m=math.pow(k,1/3)
     n=-math.pow(abs(l),1/3)
     equ=[]
+
     i=0
     while i<=2:
-	x=(w**i)*m +(w**(3-i))*n-a/3
-	equ.append(x)
-	i=i+1
+        x=(w**i)*m +(w**(3-i))*n-a/3
+        equ.append(x)
+        i=i+1
     return equ
