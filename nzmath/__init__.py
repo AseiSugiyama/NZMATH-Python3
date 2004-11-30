d297d0035b507cd352a11b53725bba2eea8fe03d
MATSUI Tetsushi <mft@users.sourceforge.net>
2004-11-30 08:17:05 +0000

    import every module

--- nzmath/__init__.py
+++ nzmath/__init__.py
@@ -1,3 +1,22 @@
+import arith1
+import bigrandom
+import combinatorial
+import elliptic
+import factor
+import finitefield
+import gcd
+import imaginary
+import integerResidueClass
+import lattice
+import matrix
+import polynomial
+import prime
+import rational
+import rationalFunction
+import real
+import ring
+import vector
+
 __all__ = [
     "arith1",
     "bigrandom",
