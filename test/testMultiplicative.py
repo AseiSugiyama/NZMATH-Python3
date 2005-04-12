5f8a70c304887c297c4a958df9e98029f588d65e
MATSUI Tetsushi <mft@users.sourceforge.net>
2005-04-12 15:15:55 +0000

    add sigma

--- test/testMultiplicative.py
+++ test/testMultiplicative.py
@@ -3,6 +3,7 @@ import multiplicative
 
 class MultiplicativeTest (unittest.TestCase):
     def testEuler(self):
+        assert multiplicative.euler(1) == 1
         assert multiplicative.euler(101) == 100
         assert multiplicative.euler(480) == 128
         assert multiplicative.euler(3**2 * 29**3 * 43**5) == 2*3 * 28*29**2 * 42*43**4 
@@ -13,7 +14,17 @@ class MultiplicativeTest (unittest.TestCase):
         assert multiplicative.moebius(2*3*5*7*11*13) == 1
         assert multiplicative.moebius(8*987654345) == 0
         assert multiplicative.moebius(999991) == -1
-        
+
+    def testSigma(self):
+        assert multiplicative.sigma(0, 1) == 1
+        assert multiplicative.sigma(1, 1) == 1
+        assert multiplicative.sigma(1, 2) == 3
+        assert multiplicative.sigma(1, 6) == 12
+        assert multiplicative.sigma(1, 28) == 56
+        assert multiplicative.sigma(2, 1) == 1
+        assert multiplicative.sigma(2, 2) == 5
+        assert multiplicative.sigma(2, 4) == 21
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(MultiplicativeTest, "test"))
