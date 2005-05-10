683758937ad93e20585e0faddf2323b2e5e091e5
MATSUI Tetsushi <mft@users.sourceforge.net>
2005-05-10 05:02:59 +0000

    created

---
+++ test/testEquation.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import unittest
+import equation
+
+class GlobalEquationTest (unittest.TestCase):
+    def test_e1(self):
+        assert -2/3 == equation.e1([3, 2])
+
+def suite():
+    suite=unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(GlobalEquationTest, "test"))
+    return suite
+
+if __name__== '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
