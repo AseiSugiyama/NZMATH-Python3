dcc3008389f739d5ef7001ea82fb8164f7091862
HIRANO Takato <devnull@localhost>
2005-06-03 10:26:27 +0000

    construct Fundamental

--- quadratic.py
+++ quadratic.py
@@ -1,12 +1,10 @@
 import polynomial
 
-class quadratic:
-    def __init__(self,valuelist,val):
+class Quadratic:
+    def __init__(self,valuelist,root):
         if len(valuelist) == 2:
-            self.dis = val
+            self.dis = root
             self.quad = valuelist
-            self.int = self.quad[0]
-            self.alg = self.quad[1]
             if self.dis % 4 == 2:
                 self.type = 0
             elif self.dis % 4 == 3:
@@ -24,18 +22,18 @@ class quadratic:
         return return_str
     def __str__(self):
         if self.type == 0:
-            return "%s+%s*sqrt(%s)" % (self.int,self.alg,self.dis)
+            return "%s+%s*sqrt(%s)" % (self.quad[0],self.quad[1],self.dis)
         else:
-            return "%s+%s*(1+sqrt(%s))/2" % (self.int,self.alg,self.dis)
+            return "%s+%s*(1+sqrt(%s))/2" % (self.quad[0],self.quad[1],self.dis)
     def __add__(self,other):
-        return quadratic([self.int+other[0],self.alg+other[1]],self.dis)
+        return Quadratic([self.quad[0]+other[0],self.quad[1]+other[1]],self.dis)
     def __sub__(self,other):
-        return quadratic([self.int-other[0],self.alg-other[1]],self.dis)
+        return Quadratic([self.quad[0]-other[0],self.quad[1]-other[1]],self.dis)
     def __mul__(self,other):
         if self.type == 0:
-            return quadratic([self.int*other[0]+self.alg*other[1]*self.dis,self.int*other[1]+self.alg*other[0]],self.dis)
+            return Quadratic([self.quad[0]*other[0]+self.quad[1]*other[1]*self.dis,self.quad[0]*other[1]+self.quad[1]*other[0]],self.dis)
         else:
-            return quadratic([self.int*other[0]+self.alg*other[1]*(self.dis-1)/4,self.int*other[1]+self.alg*other[0]+self.alg*other[1]],self.dis)
+            return Quadratic([self.quad[0]*other[0]+self.quad[1]*other[1]*(self.dis-1)/4,self.quad[0]*other[1]+self.quad[1]*other[0]+self.quad[1]*other[1]],self.dis)
     def __getitem__(self,index):
         return self.quad[index]
     def __setitem__(self,index,value):
@@ -48,3 +46,38 @@ class quadratic:
     def poly(self,variable):
         x = '%s' % (variable)
         return polynomial.OneVariableDensePolynomial([-self.dis,0,1],x)
+    def translation(self):
+        if self.type == 0:
+            return FundamentalQuadratic([self.quad[0]-2*self.quad[1]*self.dis,self.quad[1]],self.dis*4)
+        else:
+            return FundamentalQuadratic([self.quad[0]+self.quad[1]*(1-self.dis)/2,self.quad[1]],self.dis)
+
+class FundamentalQuadratic(Quadratic):
+    def __init__(self,Valuelist,Root):
+        if len(Valuelist) == 2:
+            self.dis = Root
+            self.quad = Valuelist
+            if self.dis % 4 == 0:
+                if self.dis/4 == 1:
+                    raise ValueError
+                elif self.dis/4 == 0:
+                    raise ValueError
+            elif self.dis % 4 == 1:
+                raise ValueError
+        else:
+            raise ValueError
+    def __str__(self):
+        return "%s+%s*(%s+sqrt(%s))/2" % (self.quad[0],self.quad[1],self.dis,self.dis) 
+    def __add__(self,other):
+        return FundamentalQuadratic([self.quad[0]+other[0],self.quad[1]+other[1]],self.dis)
+    def __sub__(self,other):
+        return FundamentalQuadratic([self.quad[0]-other[0],self.quad[1]-other[1]],self.dis)
+    def __mul__(self,other):
+        return FundamentalQuadratic([self.quad[0]*other[0]+self.quad[1]*other[1]*(self.dis-self.dis**2)/4,self.quad*other[1]+self.quad[1]*other[1]+self.quad[1]*other[1]*self.dis],self.dis)
+    def translation(self):
+        if self.dis % 4 == 1:
+            return Quadratic([self.quad[0]+self.quad[1]*(self.dis-1)/2,self.quad[1]],self.dis)
+        else:
+            return Quadratic([self.quad[0]+2*self.quad[1]*self.dis/4,self.quad[1]],self.dis/4)
+    def Discriminant(self):
+        return self.dis
