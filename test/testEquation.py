db9111a8a149d60f36e03eb3c856c79630c31227
MATSUI Tetsushi <mft@users.sourceforge.net>
2005-09-24 08:07:59 +0000

    BugTrack/9

--- test/testEquation.py
+++ test/testEquation.py
@@ -4,13 +4,17 @@ import equation
 
 class GlobalEquationTest (unittest.TestCase):
     def test_e1(self):
-        assert -3/2 == equation.e1([3, 2])
+        self.assertEqual(-3/2, equation.e1([3, 2]))
+
+    def test_e3(self):
+        solutions = equation.e3([1, 0, 0, 1])
+        self.assertAlmostEqual(0, abs(solutions[0] + 1))
 
 def suite():
-    suite=unittest.TestSuite()
+    suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(GlobalEquationTest, "test"))
     return suite
 
-if __name__== '__main__':
+if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     runner.run(suite())
