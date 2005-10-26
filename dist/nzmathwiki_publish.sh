4d96077b637b576bf0c22f8b34a70e4749bf9d0d
TANAKA Satoru <salt3des@users.sourceforge.net>
2005-10-26 11:03:28 +0000

    - support for module class documents. TODO: - code cleanup.

--- dist/nzmathwiki_publish.sh
+++ dist/nzmathwiki_publish.sh
@@ -26,17 +26,67 @@ do
   done
   cd ../
 done
+# 1.2. get module class document.
+for docs in finitefield
+do
+  for clses in FiniteField FinitePrimeField FiniteExtendedField FiniteFieldElement FinitePrimeFieldElement FiniteExtendedFieldElement
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in imaginary
+do
+  for clses in Complex ComplexField AbsoluteError RelativeError ExponentialPowerSeries
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in integerResidueClass
+do
+  for clses in IntegerResidueClass IntegerResidueClassRing
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in polynomial
+do
+  for clses in MultiVariableSparsePolynomial OneVariablePolynomial OneVariablePolynomialChar0 RationalOneVariablePolynomial OneVariablePolynomialCharNonZero OneVariablePolynomialCoefficients PolynomialRing MultiVariablePolynomialIdeal OneVariablePolynomialIdeal PolynomialResidueRing
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in rational
+do
+  for clses in Integer IntegerRing Rational RationalField
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in rationalFunction
+do
+  for clses in RationalFunction RationalFunctionField
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
+for docs in real
+do
+  for clses in RealField Constant ExponentialPowerSeries AbsoluteError RelativeError
+  do
+    wget -q $WIKIBASE\?$docs.py%2F$clses -O $docs\_$clses.html
+  done
+done
 
 # 2. edit document.
 cd ../
 CSSBDAT="skin\/default\.ja\.css"
 CSSNDAT="default.css"
 # 2.1. edit index.html .
-CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\)\(.*\.\)\(py\)\" "
-CONVNDAT="href=\"modules\/\2html\" "
+CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(.*\.\)py\" "
+CONVNDAT="href=\"modules\/\1html\" "
 
 cat index.html|sed -e "s/$CONVBDAT/$CONVNDAT/">index.html.sed
-CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?Install\)\" "
+CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?Install\" "
 CONVNDAT="href=\"install.html\" "
 cat index.html.sed|sed -e "s/$CSSBDAT/$CSSNDAT/">index.html.se
 cat index.html.se|sed -e "s/$CSSBDAT/$CSSNDAT/">index.html.sed
@@ -46,7 +96,7 @@ echo "you must fix index.html for modules/factor ."
 
 # 2.2. edit all html link.
 # 2.2.1 edit install.html .
-CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\)\" "
+CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\" "
 CONVNDAT="href=\"index.html\" "
 cat install.html|sed -e "s/$CONVBDAT/$CONVNDAT/">install.html.sed
 cat install.html.sed|sed -e "s/$CSSBDAT/$CSSNDAT/">install.html
@@ -57,13 +107,26 @@ cd modules
 CSSNDAT="..\/$CSSNDAT"
 for htmlfile in *.html
 do
-  CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\)\(.*\.\)\(py\)\" "
-  CONVNDAT="href=\"\2html\" "
-  cat $htmlfile|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.sed
-  CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\)\(.*\.\)\(py\)#\(.*\)\" "
-  CONVNDAT="href=\"\2html#\4\" "
-  cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.se
-  CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\)\" "
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\.\)py\)\" title=\"\1\""
+  CONVNDAT="href=\"\2html\" title=\"\1\""
+  sed -e "s/$CONVBDAT/$CONVNDAT/g" $htmlfile>$htmlfile.sed
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\.\)py\)#\(.*\)\" title=\"\1\""
+  CONVNDAT="href=\"\2html#\3\" title=\"\1\""
+  sed -e "s/$CONVBDAT/$CONVNDAT/g" $htmlfile.sed>$htmlfile.se
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\.\)py\)#\(.*\)\" title=\"\3\""
+  CONVNDAT="href=\"\2html#\3\" title=\"\3\""
+  sed -e "s/$CONVBDAT/$CONVNDAT/g" $htmlfile.se>$htmlfile.sed
+
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\)\.py\)%2F\(.*\)\" title=\"\1\/\3\""
+  CONVNDAT="href=\"\2_\3.html\" title=\"\1\/\3\""
+  cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.se
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\)\.py\)%2F\(.*\)#\(.*\)\" title=\"\4\""
+  CONVNDAT="href=\"\2_\3.html#\4\" title=\"\4\""
+  cat $htmlfile.se|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.sed
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(\(.*\)\.py\)%2F\(.*\)#\(.*\)\" title=\"\1\/\3\""
+  CONVNDAT="href=\"\2_\3.html#\4\" title=\"\1\/\3\""
+  cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.se
+  CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\" "
   CONVNDAT="href=\"..\/index.html\" "
   cat $htmlfile.se|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.sed
   cat $htmlfile.sed|sed -e "s/$CSSBDAT/$CSSNDAT/">$htmlfile
@@ -71,20 +134,26 @@ do
 done
 
 # 2.2.3. edit submodule cores .
+CSSNDAT="..\/$CSSNDAT"
 for subs in factor
 do
   cd $subs
-  CSSNDAT="..\/$CSSNDAT"
   for htmlfile in *.html
   do
-    CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\)\(.*\.\)\(py\)\" "
-    CONVNDAT="href=\"\2html\" "
-    cat $htmlfile|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.sed
-    CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\)\(.*\.\)\(py\)#\(.*\)\" "
-    CONVNDAT="href=\"..\/\2html#\4\" "
-    cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.se
-    CONVBDAT="href=\"\(http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\)\" "
-    CONVNDAT="href=\"..\/index.html\" "
+    CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(.*\.\)py\" "
+    CONVNDAT="href=\"..\/\1html\" "
+    cat $htmlfile|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.sed
+    CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(.*\.\)\{1\}py#\(.*\)\" "
+    CONVNDAT="href=\"..\/\1html#\2\" "
+    cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.se
+    CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(.*\)\.py%2F\(.*\)\" "
+    CONVNDAT="href=\"..\/\1_\2.html\" "
+    cat $htmlfile.se|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.sed
+    CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?\(.*\)\.py%2F\(.*\)#\(.*\)\" "
+    CONVNDAT="href=\"..\/\1_\2.html#\3\" "
+    cat $htmlfile.sed|sed -e "s/$CONVBDAT/$CONVNDAT/g">$htmlfile.se
+    CONVBDAT="href=\"http:\/\/hanaya\.math\.metro-u\.ac\.jp\/nzmath\/?UserManual\" "
+    CONVNDAT="href=\"..\/..\/index.html\" "
     cat $htmlfile.se|sed -e "s/$CONVBDAT/$CONVNDAT/">$htmlfile.sed
     cat $htmlfile.sed|sed -e "s/$CSSBDAT/$CSSNDAT/">$htmlfile
   done
