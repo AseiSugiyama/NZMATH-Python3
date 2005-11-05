94e579c8e478b389f7e9fa1ed9d5d32489aca9a0
MATSUI Tetsushi <mft@users.sourceforge.net>
2005-11-05 13:56:41 +0000

    use new modules

--- nzmath/factor/__init__.py
+++ nzmath/factor/__init__.py
@@ -1,22 +1,11 @@
-from trialdivision import *
-from factor import *
-from mpqs import *
+"""
+factor sub-package provides functions for integer factorizations.
+"""
+
 __all__ = [
-           "trialDivision",
-
-           "subrhomethod",
-           "rhomethod",
-           "pmom",
-           "subpmom",
-           "primeDivisors",
-           "squarePart",
-           "PrimePowerTest",
-           "AllDivisors"
-           "mod_sqrt",
-
-           "mpqs",
-           "eratosthenes",
-           "prime_mod8",
-           "eratosthenes_log",
-           "sqrt_modn",
-           "sqroot_power"]
+    "find",
+    "methods",
+    "misc",
+    "mpqs",
+    "util"
+    ]
