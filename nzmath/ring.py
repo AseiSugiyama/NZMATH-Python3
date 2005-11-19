520e13a8123bfbd611a1b9819da104e2da92d467
MATSUI Tetsushi <mft@users.sourceforge.net>
2005-11-19 18:03:13 +0000

    doc string update

--- nzmath/ring.py
+++ nzmath/ring.py
@@ -1,6 +1,9 @@
+"""
+base classes for rings.
+"""
+
 class Ring (object):
     """
-
     Ring is an abstract class which expresses that
     the derived classes are (in mathematical meaning) rings.
 
@@ -8,7 +11,6 @@ class Ring (object):
       Ring is a structure with addition and multiplication.  It is an
       abelian group with addition, and a monoid with multiplication.
       The multiplication obeys the distributive law.
-
     """
 
     def __init__(self, *args, **kwd):
@@ -18,29 +20,29 @@ class Ring (object):
         raise NotImplementedError
 
     def createElement(self, seed):
-        """createElement returns an element of the ring with seed."""
+        """
+        createElement returns an element of the ring with seed.
+        """
         raise NotImplementedError
 
     def issubring(self, other):
         """
-
         Report whether another ring contains the ring as a subring.
-
         """
         raise NotImplementedError
 
     def issuperring(self, other):
         """
-
         Report whether the ring is a superring of another ring.
-
         """
         raise NotImplementedError
 
 
 class CommutativeRing (Ring):
-    """CommutativeRing is an abstract subclass of Ring
-    whose multiplication is commutative."""
+    """
+    CommutativeRing is an abstract subclass of Ring
+    whose multiplication is commutative.
+    """
 
     def __init__(self, *args, **kwd):
         """
@@ -51,60 +53,60 @@ class CommutativeRing (Ring):
         self.properties = CommutativeRingProperties()
 
     def getQuotientField(self):
-        """getQuotientField returns the quotient field of the ring
-        if available, otherwise raises exception."""
+        """
+        getQuotientField returns the quotient field of the ring
+        if available, otherwise raises exception.
+        """
         raise NotImplementedError
 
     def isdomain(self):
-        """isdomain returns True if the ring is actually a domain,
-        False if not, or None if uncertain."""
+        """
+        isdomain returns True if the ring is actually a domain,
+        False if not, or None if uncertain.
+        """
         return self.properties.isdomain()
 
     def isnoetherian(self):
         """
-
         isnoetherian returns True if the ring is actually a Noetherian
         domain, False if not, or None if uncertain.
-
         """
         return self.properties.isnoetherian()
 
     def isufd(self):
         """
-
         isufd returns True if the ring is actually a unique
         factorization domain, False if not, or None if uncertain.
-
         """
         return self.properties.isufd()
 
     def ispid(self):
         """
-
         ispid returns True if the ring is actually a principal
         ideal domain, False if not, or None if uncertain.
-
         """
         return self.properties.ispid()
 
     def iseuclidean(self):
         """
-
         iseuclidean returns True if the ring is actually a Euclidean
         domain, False if not, or None if uncertain.
-
         """
         return self.properties.iseuclidean()
 
     def isfield(self):
-        """isfield returns True if the ring is actually a field,
-        False if not, or None if uncertain."""
+        """
+        isfield returns True if the ring is actually a field,
+        False if not, or None if uncertain.
+        """
         return self.properties.isfield()
 
 
 class Field (CommutativeRing):
-    """Field is an abstract class which expresses that
-    the derived classes are (in mathematical meaning) fields."""
+    """
+    Field is an abstract class which expresses that
+    the derived classes are (in mathematical meaning) fields.
+    """
 
     def __init__(self, *args, **kwd):
         """
@@ -116,25 +118,35 @@ class Field (CommutativeRing):
         self.properties.setIsfield(True)
 
     def createElement(self, *args):
-        """createElement returns an element of the field."""
+        """
+        createElement returns an element of the field.
+        """
         raise NotImplementedError
 
     def isfield(self):
-        """Field overrides isfield of CommutativeRing."""
+        """
+        Field overrides isfield of CommutativeRing.
+        """
         return True
 
     def gcd(self, a, b):
-        """A field is trivially a ufd and shuold be provide gcd."""
+        """
+        A field is trivially a ufd and shuold be provide gcd.
+        """
         return self.createElement(1)
 
     def getQuotientField(self):
-        """getQuotientField returns the quotient field of the field.
-        It is, of course, itself."""
+        """
+        getQuotientField returns the quotient field of the field.
+        It is, of course, itself.
+        """
         return self
 
 
 class QuotientField (Field):
-    """QuotientField is a class of quotient field."""
+    """
+    QuotientField is a class of quotient field.
+    """
 
     def __init__(self, domain):
         """
@@ -146,33 +158,45 @@ class QuotientField (Field):
 
 
 class RingElement:
-    """RingElement is an abstract class for elements of rings."""
+    """
+    RingElement is an abstract class for elements of rings.
+    """
 
     def __init__(self, *args, **kwd):
-        """This class is abstract and cannot be instanciated."""
+        """
+        This class is abstract and cannot be instanciated.
+        """
         raise NotImplementedError
 
     def getRing(self):
-        """getRing returns an object of a subclass of Ring,
-        to which the element belongs."""
+        """
+        getRing returns an object of a subclass of Ring,
+        to which the element belongs.
+        """
         raise NotImplementedError
 
 
 class CommutativeRingElement (RingElement):
+    """
+    CommutativeRingElement is an abstract class for elements of
+    commutative rings.
+    """
     pass
 
 
 class FieldElement (CommutativeRingElement):
+    """
+    FieldElement is an abstract class for elements of fields.
+    """
     pass
 
 
 class QuotientFieldElement (FieldElement):
     """
-
     QuotientFieldElement class is an abstract class to be used as a
     super class of concrete quotient field element classes.
-
     """
+
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         if denominator == 0:
@@ -216,75 +240,61 @@ class QuotientFieldElement (FieldElement):
 
 class Ideal:
     """
-
     Ideal class is an abstract class to represent the finitely
     generated ideals.  Because the finitely-generatedness is not a
     restriction for Noetherian rings and in the most cases only
     Noetherian rings are used, it is general enough.
-
     """
+
     def __init__(self, generators, ring):
         """
-
         Ideal(generators, ring) creates an ideal of the ring genarated
         by the generators.  generators must be an element of the ring
         or a list of elements of the ring.
-
         """
         raise NotImplementedError
 
     def __add__(self, other):
         """
-
         I + J <=> I.__add__(J)
 
         where I+J = {i+j | i in I and j in J}
-
         """
         raise NotImplementedError
 
     def __mul__(self, other):
         """
-
         I * J <=> I.__mul__(J)
 
         where I*J = {sum of i*j | i in I and j in J}
-
         """
         raise NotImplementedError
 
     def __eq__(self, other):
         """
-
         I == J <=> I.__eq__(J)
-
         """
         raise NotImplementedError
 
     def __ne__(self, other):
         """
-
         I != J <=> I.__ne__(J)
-
         """
         raise NotImplementedError
 
     def reduce(self, element):
         """
-
         Reduce an element with the ideal to simpler representative.
-
         """
         raise NotImplementedError
 
 
 class ResidueClassRing (CommutativeRing):
     """
-
     A residue class ring R/I,
     where R is a commutative ring and I is its ideal.
-
     """
+
     def __init__(self, ring, ideal):
         """
         ResidueClassRing(ring, ideal) creates a resudue class ring.
@@ -317,11 +327,10 @@ class ResidueClassRing (CommutativeRing):
 
 class ResidueClass (CommutativeRingElement):
     """
-
     Element of residue class ring x+I, where I is the modulus ideal
     and x is a representative element.
-
     """
+
     def __init__(self, x, ideal):
         self.x = x
         self.ideal = ideal
@@ -339,19 +348,22 @@ class ResidueClass (CommutativeRingElement):
         return self.__class__(self.ideal.reduce(self.x * other.x), self.ideal)
 
     def getRing(self):
+        """
+        Return a ResidueClassRing object.
+        This overrides the method inherited from CommutativeRingElement.
+        """
         return ResidueClassRing(self.ideal.ring, self.ideal)
 
 
 class CommutativeRingProperties:
     """
-
     boolean properties of ring.
 
     Each property can have one of three values; True, False, or None.
     Of cource True is true and False is false, and None means that the
     property is not set neither directly nor indirectly.
-
     """
+
     def __init__(self):
         self._isfield = None
         self._iseuclidean = None
@@ -361,17 +373,36 @@ class CommutativeRingProperties:
         self._isdomain = None
 
     def isfield(self):
+        """
+        Return True/False according to the field flag value being set,
+        otherwise return None.
+        """
         return self._isfield
 
     def setIsfield(self, value):
+        """
+        Set True/False value to the field flag.
+        Propergation:
+          True -> euclidean
+        """
         self._isfield = bool(value)
         if self._isfield:
             self.setIseuclidean(True)
 
     def iseuclidean(self):
+        """
+        Return True/False according to the euclidean flag value being
+        set, otherwise return None.
+        """
         return self._iseuclidean
 
     def setIseuclidean(self, value):
+        """
+        Set True/False value to the euclidean flag.
+        Propergation:
+          True  -> pid
+          False -> field
+        """
         self._iseuclidean = bool(value)
         if self._iseuclidean:
             self.setIspid(True)
@@ -379,9 +410,19 @@ class CommutativeRingProperties:
             self.setIsfield(False)
 
     def ispid(self):
+        """
+        Return True/False according to the pid flag value being set,
+        otherwise return None.
+        """
         return self._ispid
 
     def setIspid(self, value):
+        """
+        Set True/False value to the pid flag.
+        Propergation:
+          True  -> ufd, noetherian
+          False -> euclidean
+        """
         self._ispid = bool(value)
         if self._ispid:
             self.setIsufd(True)
@@ -390,9 +431,19 @@ class CommutativeRingProperties:
             self.setIseuclidean(False)
 
     def isufd(self):
+        """
+        Return True/False according to the ufd flag value being set,
+        otherwise return None.
+        """
         return self._isufd
 
     def setIsufd(self, value):
+        """
+        Set True/False value to the ufd flag.
+        Propergation:
+          True  -> domain
+          False -> pid
+        """
         self._isufd = bool(value)
         if self._isufd:
             self.setIsdomain(True)
@@ -400,9 +451,19 @@ class CommutativeRingProperties:
             self.setIspid(False)
 
     def isnoetherian(self):
+        """
+        Return True/False according to the noetherian flag value being
+        set, otherwise return None.
+        """
         return self._isnoetherian
 
     def setIsnoetherian(self, value):
+        """
+        Set True/False value to the noetherian flag.
+        Propergation:
+          True  -> domain
+          False -> pid
+        """
         self._isnoetherian = bool(value)
         if self._isnoetherian:
             self.setIsdomain(True)
@@ -410,9 +471,18 @@ class CommutativeRingProperties:
             self.setIspid(False)
 
     def isdomain(self):
+        """
+        Return True/False according to the domain flag value being
+        set, otherwise return None.
+        """
         return self._isdomain
 
     def setIsdomain(self, value):
+        """
+        Set True/False value to the domain flag.
+        Propergation:
+          False  -> ufd, noetherian
+        """
         self._isdomain = bool(value)
         if not self._isdomain:
             self.setIsufd(False)
