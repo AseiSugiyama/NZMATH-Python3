6c7191e1cc75153b366b5f034a98a390cbd86c64
MATSUI Tetsushi <mft@users.sourceforge.net>
2006-02-11 13:00:24 +0000

    change to assertEqual style

--- test/testGcd.py
+++ test/testGcd.py
@@ -4,35 +4,37 @@ import nzmath.gcd as gcd
 
 class GcdTest (unittest.TestCase):
     def testGcd(self):
-        assert gcd.gcd(1, 2) == 1
-        assert gcd.gcd(2, 4) == 2
-        assert gcd.gcd(0, 10) == 10
-        assert gcd.gcd(10, 0) == 10
-        assert gcd.gcd(13, 21) == 1
+        self.assertEqual(1, gcd.gcd(1, 2))
+        self.assertEqual(2, gcd.gcd(2, 4))
+        self.assertEqual(10, gcd.gcd(0, 10))
+        self.assertEqual(10, gcd.gcd(10, 0))
+        self.assertEqual(1, gcd.gcd(13, 21))
 
     def testBinaryGcd(self):
-        assert gcd.binarygcd(1, 2) == 1
-        assert gcd.binarygcd(2, 4) == 2
-        assert gcd.binarygcd(0, 10) == 10
-        assert gcd.binarygcd(10, 0) == 10
-        assert gcd.binarygcd(13, 21) == 1
+        self.assertEqual(1, gcd.binarygcd(1, 2))
+        self.assertEqual(2, gcd.binarygcd(2, 4))
+        self.assertEqual(10, gcd.binarygcd(0, 10))
+        self.assertEqual(10, gcd.binarygcd(10, 0))
+        self.assertEqual(1, gcd.binarygcd(13, 21))
 
     def testLcm(self):
-        assert gcd.lcm(1, 2) == 2
-        assert gcd.lcm(2, 4) == 4
-        assert gcd.lcm(0, 10) == 0
-        assert gcd.lcm(10, 0) == 0
-        assert gcd.lcm(13, 21) == 273
+        self.assertEqual(2, gcd.lcm(1, 2))
+        self.assertEqual(4, gcd.lcm(2, 4))
+        self.assertEqual(0, gcd.lcm(0, 10))
+        self.assertEqual(0, gcd.lcm(10, 0))
+        self.assertEqual(273, gcd.lcm(13, 21))
 
     def testExtgcd(self):
-        assert (-4,3,1) == gcd.extgcd(8, 11)
+        self.assertEqual((-4,3,1), gcd.extgcd(8, 11))
         extgcd = gcd.extgcd(8, 11)
-        assert 8 * extgcd[0] + 11 * extgcd[1] == extgcd[2]
+        self.assert_(8 * extgcd[0] + 11 * extgcd[1] == extgcd[2])
         import nzmath.rational as rational
-        assert (-4,3,1) == gcd.extgcd(rational.Integer(8), 11)
+        self.assertEqual((-4,3,1), gcd.extgcd(rational.Integer(8), 11))
 
     def testGcdOfList(self):
-        assert [1, [-4, 3]] == gcd.gcd_of_list([8, 11])
+        self.assertEqual([8, [1]], gcd.gcd_of_list([8]))
+        self.assertEqual([1, [-4, 3]], gcd.gcd_of_list([8, 11]))
+        self.assertEqual([1, [-4, 3, 0, 0, 0, 0, 0]], gcd.gcd_of_list([8, 11, 10, 9 ,6, 5, 4]))
 
 def suite():
     suite = unittest.makeSuite(GcdTest, "test")
