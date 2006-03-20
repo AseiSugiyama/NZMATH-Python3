9b5ba8c687640037b81639f50a203dcd4498e4d4
MATSUI Tetsushi <mft@users.sourceforge.net>
2006-03-20 06:24:14 +0000

    regression of matrix change

--- test/testPolynomial.py
+++ test/testPolynomial.py
@@ -439,7 +439,7 @@ class PolynomialGCDTest(unittest.TestCase):
         import nzmath.matrix as matrix
         self.f = OneVariableDensePolynomial([1,2,3,4,5], x)
         self.g = OneVariableDensePolynomial([7,8,9], x)
-        self.correctResult = matrix.Matrix(6,6,
+        self.correctResult = matrix.createMatrix(6,6,
                                                 [1,2,3,4,5,0]
                                             +   [0,1,2,3,4,5]
                                             +   [7,8,9,0,0,0]
