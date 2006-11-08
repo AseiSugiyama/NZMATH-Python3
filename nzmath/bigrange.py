214294d8f80d858cf89a4ea3493571e31563961b
MATSUI Tetsushi <mft@users.sourceforge.net>
2006-11-08 13:24:11 +0000

    new module

---
+++ nzmath/bigrange.py
@@ -0,0 +1,72 @@
+"""
+bigrange
+
+Generators for range like sequences.
+"""
+
+
+def count(n=0):
+    """
+    Count up infinitely from 'n' (default to 0),
+
+    see itertools.count
+    """
+    while True:
+        yield n
+        n += 1
+
+
+def range(start, stop=None, step=None):
+    """
+    Generate range like finite integer sequence but can generate more
+    than sys.maxint elements.
+    """
+    if step is None:
+        step = 1
+    elif not isinstance(step, (int, long)):
+        raise ValueError("non-integer step for finite_range()")
+    if not isinstance(start, (int, long)):
+        raise ValueError("non-integer arg 1 for finite_range()")
+    if stop is None:
+        start, stop = 0, start
+    elif not isinstance(stop, (int, long)):
+        raise ValueError("non-integer stop for finite_range()")
+
+    if step > 0:
+        n = start
+        while n < stop:
+            yield n
+            n += step
+    elif step < 0:
+        n = start
+        while n > stop:
+            yield n
+            n += step
+    else:
+        raise ValueError("zero step for finite_range()")
+
+
+def arithmetic_progression(init, difference):
+    """
+    Generate an arithmetic progression start form 'init' and
+    'difference' step.
+    """
+    return _iterate(lambda x: x + difference, init)
+
+
+def geometric_progression(init, ratio):
+    """
+    Generate a geometric progression start form 'init' and multiplying
+    'ratio'.
+    """
+    return _iterate(lambda x: x * ratio, init)
+
+
+def _iterate(func, init):
+    """
+    Generate (infinitely) a sequence (init, func(init), func(func(init)), ...)
+    """
+    val = init
+    while True:
+        yield val
+        val = func(val)
---
+++ test/testBigrange.py
@@ -0,0 +1,38 @@
+import unittest
+import itertools
+from sys import maxint
+import nzmath.bigrange as bigrange
+
+
+class CountTest (unittest.TestCase):
+    def testCount(self):
+        from_maxint = [maxint, maxint+1, maxint+2]
+        self.assertEqual(from_maxint,
+                         [i for i in itertools.islice(bigrange.count(maxint), 3)])
+        # otoh, itertools.count
+        self.assertNotEqual(from_maxint,
+                         [i for i in itertools.islice(itertools.count(maxint), 3)])
+
+
+class ProgressionTest (unittest.TestCase):
+    def testArithmeticProgression(self):
+        from_maxint = [maxint, maxint+1, maxint+2]
+        self.assertEqual(from_maxint,
+                         [i for i in itertools.islice(bigrange.arithmetic_progression(maxint, 1), 3)])
+
+    def testGeometricProgression(self):
+        self.assertEqual([3, 6, 12, 24],
+                         [i for i in itertools.islice(bigrange.geometric_progression(3, 2), 4)])
+
+
+def suite(suffix="Test"):
+    suite = unittest.TestSuite()
+    all_names = globals()
+    for name in all_names:
+        if name.endswith(suffix):
+            suite.addTest(unittest.makeSuite(all_names[name], "test"))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
