60a3e3976c742de10d802ce90dbff43ebca7ff41
KOIZUMI Masahiro <ko_izu_mi@users.sourceforge.net>
2008-03-13 00:44:38 +0000

    modify testLLL

--- test/testLattice.py
+++ test/testLattice.py
@@ -5,9 +5,9 @@ import nzmath.matrix as matrix
 class LatticeTest (unittest.TestCase):
     def testLLL(self):
         basis = matrix.createMatrix(3,3,[1,2,3,6,2,5,0,-1,2])
-        qForm = matrix.createMatrix(3,3,[7,3,0,2,6,0,2,1,2])
-        L = lattice.Lattice(basis, qForm)
-        assert L.LLL()
+        #qForm = matrix.createMatrix(3,3,[7,3,0,2,6,0,2,1,2])
+        #L = lattice.Lattice(basis, qForm)
+        assert lattice.LLL(basis)
 
 def suite():
     suite=unittest.TestSuite()
