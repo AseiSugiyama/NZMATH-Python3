35febc3896c68e8c025e999ce7b499e032c878a3
MATSUI Tetsushi <mft@users.sourceforge.net>
2008-12-22 11:52:10 +0000

    add FactoredInteger (cf prime.FactoredInteger)

--- nzmath/factor/misc.py
+++ nzmath/factor/misc.py
@@ -39,33 +39,187 @@ def primePowerTest(n):
     else:
         return (n, 0)
 
-def allDivisors(n):
-    """
-    Return all factors divide n.
-    """
-    divisors = [1]
-    for p, e in methods.factor(n):
-        p_part = [p**j for j in range(1, e+1)]
-        divisors += [n*q for n in divisors for q in p_part]
-    divisors.sort()
-    return divisors
 
-def primeDivisors(n):
+class FactoredInteger(object):
     """
-    primeDivisors(n) returns the list of primes that divides n.
+    Integers with factorization information.
     """
-    result = []
-    for d, e in methods.factor(n):
-        result.append(d)
-    return result
+    def __init__(self, integer, factors=None):
+        """
+        FactoredInteger(integer [, factors])
 
-def squarePart(n):
-    """
-    squarePart(n) returns the largest integer whose
-    square divides n.
-    """
-    result = 1
-    for d, e in methods.factor(n):
-        if e >= 2:
-            result *= d ** (e//2)
-    return result
+        If factors is given, it is a dict of type {prime:exponent}
+        and the product of prime**exponent is equal to the integer.
+        Otherwise, factorization is carried out in initialization.
+        """
+        self.integer = long(integer)
+        if factors is None:
+            self.factors = dict(methods.factor(self.integer))
+        else:
+            self.factors = dict(factors)
+
+    @classmethod
+    def from_partial_factorization(cls, integer, partial):
+        """
+        Construct a new FactoredInteger object from partial
+        factorization information given as dict of type
+        {prime:exponent}.
+        """
+        partial_factor = 1
+        for p, e in partial.iteritems():
+            partial_factor *= p**e
+        assert not integer % partial_factor, "wrong factorization"
+        return cls(integer // partial_factor) * cls(partial_factor, partial)
+
+    def __mul__(self, other):
+        if isinstance(other, FactoredInteger):
+            integer = self.integer * other.integer
+            new_factors = self.factors.copy()
+            for p in other.factors:
+                new_factors[p] = new_factors.get(p, 0) + other.factors[p]
+            return self.__class__(integer, new_factors)
+        else:
+            return self * FactoredInteger(other)
+
+    __rmul__ = __mul__
+
+    def __pow__(self, other):
+        new_integer = self.integer**other
+        new_factors = {}
+        for p in self.factors:
+            new_factors[p] = self.factors[p] * other
+        return self.__class__(new_integer, new_factors)
+
+    def __pos__(self):
+        return self.copy()
+
+    def __str__(self):
+        return str(self.integer)
+
+    def __eq__(self, other):
+        return self.integer == int(other)
+
+    def __ne__(self, other):
+        return self.integer != int(other)
+
+    def __long__(self):
+        return self.integer
+
+    __int__ = __long__
+
+    def copy(self):
+        return self.__class__(self.integer, self.factors.copy())
+
+    def __mod__(self, other):
+        # maybe you want self.is_divisible_by(other)
+        if int(other) in self.factors:
+            return 0
+        return self.integer % int(other)
+
+    def is_divisible_by(self, other):
+        """
+        Return True if other divides self.
+        """
+        if int(other) in self.factors:
+            # other is prime and divides
+            return True
+        return not self.integer % int(other)
+
+    def exact_division(self, other):
+        """
+        Divide by a factor.
+        """
+        divisor = int(other)
+        quotient = self.copy()
+        if divisor in quotient.factors:
+            if quotient.factors[divisor] == 1:
+                del quotient.factors[divisor]
+            else:
+                quotient.factors[divisor] -= 1
+        elif not isinstance(other, FactoredInteger):
+            for p, e in self.factors.iteritems():
+                while not divisor % p:
+                    divisor // p
+                    if quotient.factors[p] == 1:
+                        del quotient.factors[divisor]
+                        assert divisor % p
+                    else:
+                        quotient.factors[divisor] -= 1
+                if divisor == 1:
+                    break
+            assert divisor == 1
+        else:
+            for p, e in other.factors.iteritems():
+                assert p in quotient.factors and quotient.factors[p] >= e
+                if quotient.factors[p] == e:
+                    del quotient.factors[divisor]
+                else:
+                    quotient.factors[divisor] -= e
+        return quotient
+
+    # maybe this is what you want, isn't it?
+    __floordiv__ = exact_division
+
+    def divisors(self):
+        """
+        Return all divisors.
+        """
+        l = [1]
+        for p, e in self.factors.iteritems():
+            for j in range(1, e + 1):
+                l += [k*pow(p, j) for k in l if k % p]
+        l.sort()
+        return l
+
+    def proper_divisors(self):
+        """
+        Return the proper divisors (divisors of n excluding 1 and n).
+        """
+        l = self.divisors()
+        l.remove(1)
+        l.remove(self.integer)
+        return self.divisors()[1:-1]
+
+    def prime_divisors(self):
+        """
+        Return the list of primes that divides the number.
+        """
+        return self.factors.keys()
+
+    def square_part(self, asfactored=False):
+        """
+        Return the largest integer whose square divides the number.
+
+        If an optional argument asfactored is true, then the result is
+        also a FactoredInteger object. (default is False)
+        """
+        result = FactoredInteger(1, {})
+        for d, e in self.factors.iteritems():
+            if e >= 2:
+                result *= FactoredInteger(d ** (e//2), {d:e//2})
+        if asfactored:
+            return result
+        else:
+            return result.integer
+
+    def squarefree_part(self, asfactored=False):
+        """
+        Return the largest divisor of the number which is squarefree.
+
+        If an optional argument asfactored is true, then the result is
+        also a FactoredInteger object. (default is False)
+        """
+        result = FactoredInteger(1, {})
+        for d, e in self.factors.iteritems():
+            if e % 2:
+                result *= FactoredInteger(d, {d:1})
+        if asfactored:
+            return result
+        else:
+            return result.integer
+
+
+# for backward compatibility
+allDivisors = lambda n: FactoredInteger(n).divisors()
+primeDivisors = lambda n: FactoredInteger(n).prime_divisors()
+squarePart = lambda n: FactoredInteger(n).square_part()
--- test/testFactorMisc.py
+++ test/testFactorMisc.py
@@ -2,15 +2,18 @@ import unittest
 import nzmath.factor.misc as misc
 
 
-class MiscTest (unittest.TestCase):
+class MiscTest(unittest.TestCase):
     def testPrimeDivisors(self):
+        # backward compatibility
         self.assertEqual([3, 5], misc.primeDivisors(15))
 
     def testSquarePart(self):
+        # backward compatibility
         self.assertEqual(1, misc.squarePart(15))
         self.assertEqual(17, misc.squarePart(17**2 * 19))
 
     def testAllDivisors(self):
+        # backward compatibility
         self.assertEqual([1], misc.allDivisors(1))
         self.assertEqual([1, 2], misc.allDivisors(2))
         self.assertEqual([1, 2, 4], misc.allDivisors(4))
@@ -22,6 +25,8 @@ class MiscTest (unittest.TestCase):
         # not a power
         self.assertEqual((12, 0), misc.primePowerTest(12))
         self.assertEqual((53, 1), misc.primePowerTest(53))
+        # not a prime power
+        self.assertEqual((36, 0), misc.primePowerTest(36))
         # powers
         self.assertEqual((7, 2), misc.primePowerTest(49))
         self.assertEqual((3, 4), misc.primePowerTest(81))
@@ -29,8 +34,69 @@ class MiscTest (unittest.TestCase):
         self.assertEqual((2, 7), misc.primePowerTest(128))
 
 
-def suite():
-    suite = unittest.makeSuite(MiscTest, 'test');
+class FactoredIntegerTest(unittest.TestCase):
+    def testEquality(self):
+        fifteen = misc.FactoredInteger(15)
+        self.assertEqual(15, fifteen)
+        self.assertEqual(misc.FactoredInteger(15), fifteen)
+        self.assertNotEqual(misc.FactoredInteger(16), fifteen)
+
+    def testPrimeDivisors(self):
+        fifteen = misc.FactoredInteger(15)
+        self.assertEqual([3, 5], fifteen.prime_divisors())
+
+    def testDivisors(self):
+        fifteen = misc.FactoredInteger(15)
+        self.assertEqual([1, 3, 5, 15], fifteen.divisors())
+
+    def testProperDivisors(self):
+        fortyfive = misc.FactoredInteger(45)
+        self.assertEqual([3, 5, 9, 15], fortyfive.proper_divisors())
+
+    def testSquarePart(self):
+        fifteen = misc.FactoredInteger(15)
+        self.assertEqual(1, fifteen.square_part())
+        self.assert_(isinstance(fifteen.square_part(True), misc.FactoredInteger))
+        self.assertEqual(1, fifteen.square_part(True).integer)
+        self.assertEqual({}, fifteen.square_part(True).factors)
+        factored45 = misc.FactoredInteger(45, {3:2, 5:1})
+        self.assertEqual({3:1}, factored45.square_part(True).factors)
+
+    def testSquarefreePart(self):
+        fifteen = misc.FactoredInteger(15)
+        self.assertEqual(15, fifteen.squarefree_part())
+        self.assert_(isinstance(fifteen.squarefree_part(True), misc.FactoredInteger))
+        self.assertEqual(15, fifteen.squarefree_part(True).integer)
+        self.assertEqual({3:1, 5:1}, fifteen.squarefree_part(True).factors)
+        factored45 = misc.FactoredInteger(45, {3:2, 5:1})
+        self.assertEqual({5:1}, factored45.squarefree_part(True).factors)
+
+    def testInitFactored(self):
+        factored45 = misc.FactoredInteger(45, {3:2, 5:1})
+        self.assertEqual(45, factored45.integer)
+        self.assertEqual({3:2, 5:1}, factored45.factors)
+
+    def testPartial(self):
+        partial45 = misc.FactoredInteger.from_partial_factorization(45, {3:1})
+        self.assertEqual(45, partial45.integer)
+        self.assertEqual({3:2, 5:1}, partial45.factors)
+
+    def testIsDivisibleBy(self):
+        factored45 = misc.FactoredInteger(45, {3:2, 5:1})
+        self.failIf(factored45.is_divisible_by(2))
+        self.assert_(factored45.is_divisible_by(3))
+        self.assert_(factored45.is_divisible_by(5))
+        self.failIf(factored45.is_divisible_by(7))
+        self.assert_(factored45.is_divisible_by(9))
+        self.assert_(factored45.is_divisible_by(15))
+
+
+def suite(suffix="Test"):
+    suite = unittest.TestSuite()
+    all_names = globals()
+    for name in all_names:
+        if name.endswith(suffix):
+            suite.addTest(unittest.makeSuite(all_names[name], "test"))
     return suite
 
 if __name__ == '__main__':
