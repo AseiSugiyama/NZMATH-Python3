806bceac84ab9737aa788adf0af9027d9af7aec9
naoki <naoki@MYPC>
2009-09-26 14:56:56 +0900

    rename & sorting

--- nzmath/arygcd.py
+++ nzmath/arygcd.py
@@ -1,4 +1,19 @@
+"""
+binary-like gcd algorithms for rational, gauss, and eisenstein integer
+"""
+
 import math
+
+
+def bit_num(a):
+    """
+    return the number of bits
+    """
+    if a == 0:
+        return 1
+    else:
+        return int(math.log(a, 2)) + 1
+
 
 def binarygcd(a, b):
     """
@@ -50,20 +65,46 @@ def arygcd_i(a1, a2, b1, b2):
     while a1 != 0 or a2 != 0:
         while (a1-a2) & 1 == 0:
             a1, a2 = (a1+a2) >> 1, (a2-a1) >> 1
-        norm_a, norm_b = ap_norm_g(a1, a2, b1, b2)
+        norm_a, norm_b = _ap_norm_i(a1, a2, b1, b2)
         if norm_a < norm_b:
             a1, a2, b1, b2 = b1, b2, a1, a2
-        a1, a2 = FormAdj_i(a1, a2)
-        b1, b2 = FormAdj_i(b1, b2)
+        a1, a2 = _FormAdj_i(a1, a2)
+        b1, b2 = _FormAdj_i(b1, b2)
         a1, a2 = a1-b1, a2-b2
     if 0 in (ap, bp):
         return b1, b2
     else:
-        s, t = n_pow_i(1, 1, k)
+        s, t = _n_pow_i(1, 1, k)
         return (b1*s)-(b2*t), (b1*t)+(b2*s)
 
+def _ap_norm_i(a, b, c, d):
+    """
+    Return approximately norm of 2 gaussInteger a+b*i and c+d*i  
+    """
+    a, b, c, d = abs(a), abs(b), abs(c), abs(d)
+    max_dig = max(bit_num(a), bit_num(b), bit_num(c), bit_num(d))
+    if max_dig > 6:
+        sft_num = max_dig - 6
+        a, b, c, d = a >> sft_num, b >> sft_num, c >> sft_num, d >> sft_num
+        return a*a + b*b, c*c + d*d
+    else:
+        return a*a + b*b, c*c + d*d
 
-def FormAdj_i(a, b):
+def _n_pow_i(a, b, n):
+    """
+    return (1+i)**k 
+    """
+    x = a
+    y = b
+
+    for i in range(1, n):
+        x1 = (x*a) - (y*b)
+        y1 = (y*a) + (x*b)
+        x = x1
+        y = y1
+    return x, y
+
+def _FormAdj_i(a, b):
     """
     transform gaussInteger a+b*i ->  form 1+2(1+i)*(x+y*i)
     """
@@ -95,16 +136,30 @@ def arygcd_w(a1, a2, b1, b2):
     while a1 != 0 or a2 != 0:
         while (a1 + a2) % 3 == 0:
             a1, a2 = (a1 + a1 - a2) / 3, (a1 + a2) / 3
-        nrm_a, nrm_b = ap_norm(a1, a2, b1, b2)
+        nrm_a, nrm_b = _ap_norm_w(a1, a2, b1, b2)
         if nrm_a < nrm_b:
             a1, a2, b1, b2 = b1, b2, a1, a2
-        a1, a2 = FormAdj_w(a1, a2)
-        b1, b2 = FormAdj_w(b1, b2)
+        a1, a2 = _FormAdj_w(a1, a2)
+        b1, b2 = _FormAdj_w(b1, b2)
         a1, a2 = a1 - b1, a2 - b2
-    k1, k2 = n_pow_w(k)
+    k1, k2 = _n_pow_w(k)
     return k1*b1 - k2*b2, k1*b2 + k2*b1 - k2*b2
 
-def n_pow_w(n):
+def _ap_norm_w(a, b, c, d):
+    """
+    Return approximately norm of 2 eisensteinInteger a+b*w and c+d*w  
+    """
+    a, b, c, d = abs(a), abs(b), abs(c), abs(d)
+    max_dig = max(bit_num(a), bit_num(b), bit_num(c), bit_num(d))
+    if max_dig > 6:
+        sft_num = max_dig - 6
+        a, b, c, d = a >> sft_num, b >> sft_num, c >> sft_num, d >> sft_num
+        subst1, subst2 = (a - b) >> sft_num, (c - d) >> sft_num
+        return a*a + b*b + subst1*subst1, c*c + d*d + subst2*subst2
+    else:
+        return a*a + b*b + (a - b)*(a - b), c*c + d*d + (c -d)*(c -d)
+
+def _n_pow_w(n):
     """
     return (1-w)**k
     """
@@ -116,7 +171,7 @@ def n_pow_w(n):
         k2 = 0
     return k1, k2
 
-def FormAdj_w(a1, a2):
+def _FormAdj_w(a1, a2):
     """
     transform eisensteinInteger a1+a2*w ->  form 1+3*(x+y*w)
     """
@@ -135,51 +190,3 @@ def FormAdj_w(a1, a2):
             return -a2, a1 -a2
         else:
             return -a1, -a2
-
-
-def ap_norm(a, b, c, d):
-    """
-    Return approximately norm of 2 eisensteinInteger a+b*w and c+d*w  
-    """
-    a, b, c, d = abs(a), abs(b), abs(c), abs(d)
-    max_dig = max(dig_num(a), dig_num(b), dig_num(c), dig_num(d))
-    if max_dig > 6:
-        sft_num = max_dig - 6
-        a, b, c, d = a >> sft_num, b >> sft_num, c >> sft_num, d >> sft_num
-        subst1, subst2 = (a - b) >> sft_num, (c - d) >> sft_num
-        return a*a + b*b + subst1*subst1, c*c + d*d + subst2*subst2
-    else:
-        return a*a + b*b + (a - b)*(a - b), c*c + d*d + (c -d)*(c -d)
-
-def ap_norm_g(a, b, c, d):
-    """
-    Return approximately norm of 2 gaussInteger a+b*i and c+d*i  
-    """
-    a, b, c, d = abs(a), abs(b), abs(c), abs(d)
-    max_dig = max(dig_num(a), dig_num(b), dig_num(c), dig_num(d))
-    if max_dig > 6:
-        sft_num = max_dig - 6
-        a, b, c, d = a >> sft_num, b >> sft_num, c >> sft_num, d >> sft_num
-        return a*a + b*b, c*c + d*d
-    else:
-        return a*a + b*b, c*c + d*d
-
-def dig_num(a):
-    if a == 0:
-        return 1
-    else:
-        return int(math.log(a, 2)) + 1
-
-def n_pow_i(a, b, n):
-    """
-    return (1+i)**k 
-    """
-    x = a
-    y = b
-
-    for i in range(1, n):
-        x1 = (x*a) - (y*b)
-        y1 = (y*a) + (x*b)
-        x = x1
-        y = y1
-    return x, y
