2d1d2b40fea500e742686dad28a5c235b0a316fa
Owner <Owner@zero>
2011-07-19 02:11:41 +0900

    add functions about Fibonacci

--- nzmath/arith2.py
+++ nzmath/arith2.py
@@ -20,3 +20,61 @@ def powerDetection(n, largest_exp = False):
                 return power_root, exp
 
     return n, 1
+
+
+def generator_fibonacci(n = None):
+    """
+    Generate Fibonacci number up to n
+    """
+    a = 0
+    b = 1
+
+    if None == n:
+        while True:
+            yield b
+            a += b
+            yield a
+            b += a
+    else:
+        count = 0
+        while True:
+            yield b
+            count += 1
+            if n <= count:
+                break
+            a += b
+
+            yield a
+            count += 1
+            if n <= count:
+                break
+            b += a
+
+
+QLRST = {0:0, 1:1}
+def fibonacci(n):
+    """
+    Fibonacci Sequence
+    param positive integer n
+    return the n-th term of the QLRS
+    effect QLRST[n] = QLRS(n)
+    """
+    global PRECOMPUTED_FIBONACCI, QLRST
+
+    if n < 0:
+        raise ValueError, "fibonacci(n)  0 <= n  ?"
+
+    if n in QLRST:
+        return QLRST[n]
+
+    m = n // 2
+    if n & 1 == 0:
+        f1 = fibonacci(m - 1)
+        f2 = fibonacci(m)
+        QLRST[n] = (f1 + f1 + f2) * f2
+    else : # odd  n
+        f1 = fibonacci(m)
+        f2 = fibonacci(m + 1)
+        QLRST[n] = f1 ** 2 + f2 ** 2
+
+    return QLRST[n]
