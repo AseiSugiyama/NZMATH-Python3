4b0c47de13633a5393ef5f541477e929caa8353d
miyamoto <miyamoto@fujiya>
2011-07-19 15:29:41 +0900

    change definition position of powerDetection

--- nzmath/arith1.py
+++ nzmath/arith1.py
@@ -78,6 +78,28 @@ def floorpowerroot(n, k, return_power = False):
     else:
         return x
 
+def powerDetection(n, largest_exp = False):
+    """
+    param positive integer n
+    param boolean largest_exp
+    return integer x, k s.t. n == x ** k
+           (2 <= k if exist else x, k == n, 1)
+           if largest_exp is true then return largest k
+    """
+    from nzmath.prime import generator_eratosthenes as generator_eratosthenes
+
+    ge = generator_eratosthenes(log(n, 2))
+    for exp in ge:
+        power_root, power = floorpowerroot(n, exp, True)
+        if power == n:
+            if largest_exp:
+                x, k = powerDetection(power_root, True)
+                return x, k * exp
+            else:
+                return power_root, exp
+
+    return n, 1
+
 def legendre(a, m):
     """
     This function returns the Legendre symbol (a/m).
--- nzmath/arith2.py
+++ nzmath/arith2.py
@@ -1,30 +1,6 @@
-from nzmath.arith1 import *
-from nzmath.prime import generator_eratosthenes as generator_eratosthenes
-
-def powerDetection(n, largest_exp = False):
-    """
-    param positive integer n
-    param boolean largest_exp
-    return integer x, k s.t. n == x ** k
-           (2 <= k if exist else x, k == n, 1)
-           if largest_exp is true then return largest k
-    """
-    ge = generator_eratosthenes(log(n, 2))
-    for exp in ge:
-        power_root, power = floorpowerroot(n, exp, True)
-        if power == n:
-            if largest_exp:
-                x, k = powerDetection(power_root, True)
-                return x, k * exp
-            else:
-                return power_root, exp
-
-    return n, 1
-
-
 def generator_fibonacci(n = None):
     """
-    Generate Fibonacci number up to n
+    Generate Fibonacci number up to n-th term
     """
     a = 0
     b = 1
@@ -51,30 +27,30 @@ def generator_fibonacci(n = None):
             b += a
 
 
-QLRST = {0:0, 1:1}
+FIBONACCI = {0:0, 1:1}
 def fibonacci(n):
     """
     Fibonacci Sequence
-    param positive integer n
-    return the n-th term of the QLRS
-    effect QLRST[n] = QLRS(n)
+    param non-negative integer n
+    return the n-th term of the Fibonacci
+    effect FIBONACCI[n] = FIBONACCI(n)
     """
-    global PRECOMPUTED_FIBONACCI, QLRST
+    global FIBONACCI
 
     if n < 0:
         raise ValueError, "fibonacci(n)  0 <= n  ?"
 
-    if n in QLRST:
-        return QLRST[n]
+    if n in FIBONACCI:
+        return FIBONACCI[n]
 
     m = n // 2
     if n & 1 == 0:
         f1 = fibonacci(m - 1)
         f2 = fibonacci(m)
-        QLRST[n] = (f1 + f1 + f2) * f2
+        FIBONACCI[n] = (f1 + f1 + f2) * f2
     else : # odd  n
         f1 = fibonacci(m)
         f2 = fibonacci(m + 1)
-        QLRST[n] = f1 ** 2 + f2 ** 2
+        FIBONACCI[n] = f1 ** 2 + f2 ** 2
 
-    return QLRST[n]
+    return FIBONACCI[n]
