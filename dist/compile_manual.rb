5dc61ab76cd7897033c87b36e2083702318bc733
naoki <naoki@HASSE-Mac>
2012-01-15 23:49:49 +0900

    1: simplify header(header includes documentclass), 2: divide introduction to another file, 3: script for packaging manual sources

--- dist/compile_manual.rb
+++ dist/compile_manual.rb
@@ -1,5 +1,7 @@
 # -*- coding: SJIS -*-
 
+require 'fileutils'
+
 def is_win
 	return RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|bccwin/
 end
@@ -33,6 +35,23 @@ def clean
 	end
 end
 
+def head_foot_del(file)
+    # 1行目と最終行 (header, footer) の削除
+    data = ""
+    open(file, 'r') do |f|
+        data = f.readlines
+        lastline = data.pop
+        firstline = data.shift
+    end
+    file_bak = file + ".bak"
+    File.rename file, file_bak
+    open(file, "w") do |f|
+        f.write data
+    end
+
+    File.delete(file_bak)
+end
+
 def compile(dir)
 	savedir = Dir.pwd
 	Dir.chdir(dir)
@@ -43,10 +62,11 @@ def compile(dir)
 	# 個別ファイルコンパイル
 	Dir::glob("*.pdf").each do |file|
 		next if "nzmath_doc" == file[0...-4]
+        p file
 		tex_compile(file[0...-4])
 	end
 
-	header_footer = ["header_basic_util.tex", "header_class.tex",
+	header_footer = ["header_overview.tex", "header_basic_util.tex", "header_class.tex",
 		"header_function.tex", "footer.tex"]
 	header_footer.map!{|file| '../' + file}
 
@@ -76,5 +96,44 @@ ensure
 	Dir.chdir(savedir)
 end
 
+def source_compress(dir, olddirrm=false)
+	savedir = Dir.pwd
+	Dir.chdir(dir)
+
+    srcdir = '../manual_source/'
+    if olddirrm then
+        # 旧ディレクトリの削除       
+        if File.exists?(srcdir) then
+            FileUtils.rm_r(srcdir)
+        end
+    end
+
+    # 新しいディレクトリの作成
+    if not File.exists?(srcdir) then
+        Dir.mkdir(srcdir)
+    end
+    srctargetdir = srcdir + File.basename(dir) + '/'
+    if not File.exists?(srctargetdir) then
+        Dir.mkdir(srctargetdir)
+    end
+    
+    # ソースの移動
+    Dir::glob("*.tex").each do |file|
+        FileUtils.cp(file, srctargetdir)
+    end
+    FileUtils.cp("../macros.tex", srcdir)
+    Dir.chdir(srctargetdir)
+
+    # header, footer の処理
+    Dir::glob("*.tex").each do |file|
+        next if "nzmath_doc" == file[0...-4]
+        head_foot_del(file)
+    end
+
+    Dir.chdir(savedir)
+end
+
 compile("../manual/en/")
+source_compress("../manual/en/", true)
 compile("../manual/ja/")
+source_compress("../manual/ja/")
