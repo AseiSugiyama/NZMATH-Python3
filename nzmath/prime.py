1c6128557b77cea9572e7784316e0b5fcf865f27
MATSUI Tetsushi <mft@users.sourceforge.net>
2012-07-01 16:56:44 +0900

    flatten nested function. use plugins.MATHMODULE

--- nzmath/prime.py
+++ nzmath/prime.py
@@ -8,9 +8,9 @@ import nzmath.arith1 as arith1
 import nzmath.gcd as gcd
 import nzmath.bigrandom as bigrandom
 import nzmath.bigrange as bigrange
-from nzmath.config import GRH
-import math
 import nzmath.poly.array as array_poly
+from nzmath.config import GRH
+from nzmath.plugins import MATHMODULE as math
 
 _log = logging.getLogger('nzmath.prime')
 _log.setLevel(logging.DEBUG)
@@ -1210,7 +1210,9 @@ class ImplementLimit (Exception):
     """
     pass
 
-def aks( n ):
+
+# AKS
+def aks(n):
     """
     AKS ( Cyclotomic Conguence Test ) primality test for a nutural number.
     
@@ -1220,64 +1222,63 @@ def aks( n ):
     """
     import nzmath.multiplicative as multiplicative
 
-    def aks_mod( polynomial , r ):
-        """
-        This function is used in aks.
-        polynomial modulo ( x^r - 1 )
-        """
-        aks_mod = polynomial.coefficients
-        total = aks_mod[ : r ]
-        aks_mod = aks_mod[ r : ]
-        while len(aks_mod) - 1 >= r :
-            for i in range(r):
-                total[i] += aks_mod[i]
-            aks_mod = aks_mod[ r : ]
-        for i in range(len(aks_mod)):
-            total[i] += aks_mod[i]
-        return array_poly.array_poly_mod( total , polynomial.mod )
-
-    lg = math.log( n , 2 )
-    k = int( lg * lg )
-
-    if arith1.powerDetection( n )[ 1 ] != 1: #Power Detection
+    if arith1.powerDetection(n)[1] != 1: #Power Detection
         return False
+
+    lg = math.log(n, 2)
+    k = int(lg * lg)
 
     start = 3
     while 1:
-        d = arith1.gcd.gcd( start , n )
+        d = gcd.gcd(start, n)
         if 1 < d < n:
             return False
         x = n % start
         N = x
-        for i in range( 1 , k + 1 ):
+        for i in range(1, k + 1):
             if N == 1:
                 break
-            N = ( N * x ) % start
+            N = (N * x) % start
         if i == k:
             r = start
             break
         start += 1
-    d = arith1.gcd.gcd( r , n )
+    d = gcd.gcd(r, n)
     if 1 < d < n:
         return False
     if n <= r:
         return True
 
-    e = multiplicative.euler( r ) #Cyclotomic Conguence
-    e = math.sqrt( e )
-    e = int( e * lg )
-    for b in range( 1 , e+1 ):
-        f = array_poly.array_poly_mod( [ b , 1 ] , n )
-        total = array_poly.array_poly_mod( [ 1 ] , n )
+    e = multiplicative.euler(r) #Cyclotomic Conguence
+    e = math.sqrt(e)
+    e = int(e * lg)
+    for b in range(1, e + 1):
+        f = array_poly.array_poly_mod([b, 1], n)
+        total = array_poly.array_poly_mod([1], n)
         count = n
         while count > 0:
             if count & 1:
                 total = total * f
-                total = aks_mod( total , r )
+                total = _aks_mod(total, r)
             f = f.power()
-            f = aks_mod( f , r )
+            f = _aks_mod(f, r)
             count = count >> 1
         total_poly = total.coefficients_to_dict()
-        if total_poly != { 0 : b , n % r : 1 }:
+        if total_poly != {0:b, n % r:1}:
             return False
     return True
+
+def _aks_mod(polynomial, r):
+    """
+    This function is used in aks.
+    polynomial modulo (x^r - 1)
+    """
+    total = polynomial.coefficients[:r]
+    aks_mod = polynomial.coefficients[r:]
+    while len(aks_mod) - 1 >= r:
+        for i in range(r):
+            total[i] += aks_mod[i]
+        aks_mod = aks_mod[r:]
+    for i in range(len(aks_mod)):
+        total[i] += aks_mod[i]
+    return array_poly.array_poly_mod(total, polynomial.mod)
